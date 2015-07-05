fb113086212974c35259a064c1951535878ce6e6
junemp <junemp@gmail.com>
2015-07-05 23:09:14 +0900

    * SpringBoot Project 생성

---
+++ spring/SpringBoot-Basic/src/main/java/devlab/spring/springboot/basic/DemoApplication.java
@@ -0,0 +1,103 @@
+package devlab.spring.springboot.basic;
+
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@Component
+class BookingCommandLineRunner implements CommandLineRunner {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.boot.CommandLineRunner#run(java.lang.String[])
+	 */
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		System.out.println("CommandLineRunner");
+
+		for (Booking b : this.bookingRepository.findAll()) {
+			System.out.println(b.toString());
+		}
+		
+	}
+	
+	@Autowired BookingRepository bookingRepository;
+	
+}
+
+
+interface BookingRepository extends JpaRepository<Booking, Long> {
+	Collection<Booking> findByBookingName(String bookingName);
+}
+
+@RestController
+class BookingRestController {
+	
+	@RequestMapping("/bookings")
+	Collection<Booking> bookings() {
+		return this.bookingRepository.findAll();
+	}
+	
+	@Autowired BookingRepository bookingRepository;
+}
+
+@Entity
+class Booking {
+	@Id @GeneratedValue
+	private Long id;
+	private String bookingName;
+	
+	/**
+	 * @param bookingName
+	 */
+	public Booking(String bookingName) {
+		super();
+		this.bookingName = bookingName;
+	}
+
+	/**
+	 * 
+	 */
+	public Booking() {
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @return the bookingName
+	 */
+	public String getBookingName() {
+		return bookingName;
+	}
+	
+	
+	
+}
---
+++ spring/SpringBoot-Basic/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into booking(booking_name) values('AAA');
+insert into booking(booking_name) values('BBB');
+insert into booking(booking_name) values('CCC');
