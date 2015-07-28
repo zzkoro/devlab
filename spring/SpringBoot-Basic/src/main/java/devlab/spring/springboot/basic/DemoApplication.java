92b5f30a9d232743f8231e15bb6b1157137e2b60
zzkoro <zzkoro@gmail.com>
2015-07-28 18:32:22 +0900

    * springBoot 테스트

--- spring/SpringBoot-Basic/src/main/java/devlab/spring/springboot/basic/DemoApplication.java
+++ spring/SpringBoot-Basic/src/main/java/devlab/spring/springboot/basic/DemoApplication.java
@@ -20,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+/**
+ * 
+ * @author asus
+ *
+ */
 public class DemoApplication {
 
     public static void main(String[] args) {
